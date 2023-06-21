3e5155d7eff32844537e7a2949c731c44fc70340
Olamide <olamide@thoughtbot.com>
2023-06-21 12:09:04 +0100

    Update output description

--- customer-managed-kms/outputs.tf
+++ customer-managed-kms/outputs.tf
@@ -1,10 +1,10 @@
 output "kms_key_alias" {
-  description = "Alias of the KMS key encrypting the secret"
+  description = "Alias of the KMS key"
   value       = aws_kms_alias.this.name
 }
 
 output "kms_key_arn" {
-  description = "Alias of the KMS key encrypting the secret"
+  description = "Arn of the KMS key"
   value       = aws_kms_alias.this.arn
 }
 
